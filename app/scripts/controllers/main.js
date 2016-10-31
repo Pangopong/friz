a922bd0a14bac785737970190e79d9553ef15780
ITS-NET\petroschi.matei <email@email.com>
2016-10-31 13:12:42 +0200

    better map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -95,6 +95,22 @@ angular.module('itsApp')
             });
     });
 
+    //map
+    $(document).ready(function(){
+        // you want to enable the pointer events only on click;
+
+        $('#map').addClass('scrolloff'); // set the pointer events to none on doc ready
+        $('#section4').on('click', function () {
+            $('#map').removeClass('scrolloff'); // set the pointer events true on click
+        });
+
+        // you want to disable pointer events when the mouse leave the canvas area;
+
+        $("#map").mouseleave(function () {
+            $('#map').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
+        });
+    });
+
 
 
 
