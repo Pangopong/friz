57f3a433e13d5012e2df7e62a2b0114fa0c835ff
Lappy\Raziel <e@e.com>
2016-11-08 21:55:51 +0200

    look change

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,6 +26,26 @@ angular.module('itsApp')
         //document.getElementById("hidden") = 200;
     }
 
+    //OWL
+    $(document).ready(function() {
+ 
+        $("#owl-example").owlCarousel({
+
+            navigation : false, // Show next and prev buttons
+            slideSpeed : 300,
+            paginationSpeed : 400,
+            singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+        });
+ 
+    });
+
     //carousel 
     $scope.cbtn = function(btn){
       
